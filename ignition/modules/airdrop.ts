f3c844893c447f16ae70e45b1a091336b46b11ec
farazmn <farazmnm@gmail.com>
2025-08-23 18:07:55 +0330

    feat : create ignition for deploy airdrop Contract

--- ignition/modules/airdrop.ts
+++ ignition/modules/airdrop.ts
@@ -1,7 +1,27 @@
 import {buildModule} from '@nomicfoundation/hardhat-ignition/modules'
+import {network} from 'hardhat'
+import * as dotenv from "dotenv";
+dotenv.config();
+const {ethers} = await network.connect({
+    network: "localhost",
+})
+
+
+
+const [sender] = await ethers.getSigners();
+
+console.log(process.env.TOKEN_ADDRESS);
+
 
 export default buildModule('airdrop' , (m)=>{
-    const airdrop = m.contract("Airdrop",[])
+    const airdrop = m.contract("Airdrop",[
+        sender.address,
+        process.env.TOKEN_ADDRESS || "",
+        100,
+        10,
+        100000000000000
+    ])
+
     return{
         airdrop
     }
