c32ae4a6840f38af656727244763ff679f5e4aaf
farazmn <farazmnm@gmail.com>
2025-08-26 15:18:26 +0330

    feat: add comprehensive README documentation, implement ICO contract structure, and enhance token transfer script with detailed comments and improved functionality

---
+++ contracts/Ico/ico.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ICO {
+    uint256 public immutable totalAmount ;
+    uint256 public immutable icoAmount;
+    uint256 public immutable icoPrice;
+    IERC20 public immutable tokenAddress;
+    
+
+    
+     
+}
